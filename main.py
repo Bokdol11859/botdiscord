df24ca5bc106c26a124d27874ed55c4af1355254
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-12-23 01:26:21 +0900

    dice function implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord, asyncio, os
 from discord.ext import commands
 import json
+import random
 
 with open('config.json') as f:
     key = json.load(f)
@@ -17,6 +18,18 @@ async def on_ready():
 @bot.command(aliases = ['hi', '안녕', 'ㅎㅇ'])
 async def hello(ctx):
     await ctx.send('{}님 안녕하세요!'.format(ctx.author.name))
+
+@bot.command(aliases=['주사위'])
+async def dice(ctx):
+    dice0={1:'⚀=1',2:'⚁=2',3:'⚂=3',4:'⚃=4',5:'⚄=5',6:'⚅=6'}#딕셔너리
+    embed = discord.Embed(title="주사위 굴리는중", color=0x4432a8)
+    dice1=random.randrange(1,7)
+    dice11=dice0[dice1]
+    dice2=random.randrange(1,7)
+    dice22=dice0[dice2]
+    embed.add_field(name="1번:game_die:",value=f"{dice11}",inline=True)
+    embed.add_field(name="2번:game_die:", value=f"{dice22}",inline=True)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def copy(ctx,*,text):
