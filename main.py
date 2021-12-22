7f7dfa718fe9efbd0ed2f038f77c72797010794b
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-12-23 01:03:30 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord, asyncio, os
+from discord.ext import commands
+import json
+
+with open('../DiscordBotTest/config.json') as f:
+    key = json.load(f)
+
+game = discord.Game("Currently Testing")
+bot = commands.Bot(command_prefix='!', status=discord.Status.online, activity=game)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('{}님 안녕하세요!'.format(ctx.author.name))
+
+bot.run(key['token'])
+
+#test1fdsa
