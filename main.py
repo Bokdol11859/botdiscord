bd592fbe7f7e07aceac1b07839e3a60fe5547821
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-12-27 23:02:30 +0900

    skip function implemented

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import youtube_dl
 from discord.ext import commands
 import json
 import random
+from collections import deque
 
 #
 import discord
@@ -10,7 +11,7 @@ with open('config.json') as f:
     key = json.load(f)
 
 bot = commands.Bot(command_prefix='!')
-
+song_list = deque()
 @bot.event
 async def on_ready():
     print('로그인중입니다. ')
@@ -63,10 +64,12 @@ async def copy(ctx,*,text):
 @bot.command()
 async def play(ctx, url):
     channel = ctx.author.voice.channel
-    if bot.voice_clients == []:
+    global song_list
+    if not bot.voice_clients:
         await channel.connect()
         await ctx.send(str(bot.voice_clients[0].channel)+"채널에 연결 되었습니다.")
-
+    if bot.voice_clients[0].is_playing():
+        song_list.append(url)
     ydl_opts = {'format': 'bestaudio'}
     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -108,7 +111,15 @@ async def stop(ctx):
 async def skip(ctx):
     if bot.voice_clients[0].is_playing():
         bot.voice_clients[0].stop()
-        bot.voice_clients[0].play()
+        url = song_list.popleft()
+        ydl_opts = {'format': 'bestaudio'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                          'options': '-vn'}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            URL = info['formats'][0]['url']
+        voice = bot.voice_clients[0]
+        voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
     else:
         await ctx.send("뒤에 재생 할 음악이 존재하지 않습니다.")
 bot.run(key['token'])
